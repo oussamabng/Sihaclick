a951a51c58bf96a677007a9f046dd30fff9dada3
oussamabng <o.bengoudifa@esi-sba.dz>
2020-05-12 23:28:57 +0100

    add title card best docs

--- frontend/src/components/BestDoctors/BestDoctors.jsx
+++ frontend/src/components/BestDoctors/BestDoctors.jsx
@@ -7,13 +7,32 @@ import Pub from "../../assets/pub.png";
 export default function BestDoctors() {
   return (
     <div className="_best_doc">
+      <div className="cercle_right"></div>
+      <div className="cercle_left"></div>
       <Container>
-        <div
-          className="pub_img"
-          style={{
-            backgroundImage: `url(${Pub})`,
-          }}
-        ></div>
+        <div className="info">
+          <div
+            className="pub_img"
+            style={{
+              backgroundImage: `url(${Pub})`,
+            }}
+          ></div>
+          <div className="title_best_doc">
+            <div className="line_title"></div>
+            <p>
+              Choisir entre nos <br />
+              meilleur médecins
+            </p>
+            <div className="plus_title">
+              <div className="up"></div>
+              <div className="down"></div>
+            </div>
+          </div>
+          <h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the{" "}
+          </h3>
+        </div>
       </Container>
     </div>
   );

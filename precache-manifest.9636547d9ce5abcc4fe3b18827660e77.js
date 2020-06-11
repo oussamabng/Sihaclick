5ddd6fac9d84d2a054a42f9dc95c0b01a13ad8bb
oussamabng <o.bengoudifa@esi-sba.dz>
2020-06-11 19:01:30 +0100

    Updates

---
+++ precache-manifest.9636547d9ce5abcc4fe3b18827660e77.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d6b247df73e39cbfda8546887788fe",
+    "url": "/Sihaclick/index.html"
+  },
+  {
+    "revision": "ebda036997ecd7df7cdc",
+    "url": "/Sihaclick/static/css/2.ceda0fd2.chunk.css"
+  },
+  {
+    "revision": "5a4c991809704090100d",
+    "url": "/Sihaclick/static/css/main.f0776c20.chunk.css"
+  },
+  {
+    "revision": "ebda036997ecd7df7cdc",
+    "url": "/Sihaclick/static/js/2.959065b5.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/Sihaclick/static/js/2.959065b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4c991809704090100d",
+    "url": "/Sihaclick/static/js/main.b5720cbe.chunk.js"
+  },
+  {
+    "revision": "be056798acb019292ce7",
+    "url": "/Sihaclick/static/js/runtime-main.4af7b73e.js"
+  },
+  {
+    "revision": "3574deff5bf028ee5e87033e3aaf8eaa",
+    "url": "/Sihaclick/static/media/Medicament.3574deff.svg"
+  },
+  {
+    "revision": "754fc90a6f6f48ff714bb6550e12e7e3",
+    "url": "/Sihaclick/static/media/Pharmacies.754fc90a.png"
+  },
+  {
+    "revision": "f45e4243c644cb6e038c716a06c7ae36",
+    "url": "/Sihaclick/static/media/a.f45e4243.svg"
+  },
+  {
+    "revision": "07a6b8e44b480af01466a969e094ecb1",
+    "url": "/Sihaclick/static/media/ab.07a6b8e4.svg"
+  },
+  {
+    "revision": "a05374144a4cf8f611ec2d886c33fd08",
+    "url": "/Sihaclick/static/media/arrow.a0537414.svg"
+  },
+  {
+    "revision": "f6a2e311364e0163905e002a07cb8d1c",
+    "url": "/Sihaclick/static/media/arrow_big.f6a2e311.svg"
+  },
+  {
+    "revision": "13e4d18be045a9114846e91060482aad",
+    "url": "/Sihaclick/static/media/b.13e4d18b.svg"
+  },
+  {
+    "revision": "69bf58ae873f38736439173fb908f259",
+    "url": "/Sihaclick/static/media/blood_img.69bf58ae.svg"
+  },
+  {
+    "revision": "0d9374598fecc903eeeca85e36f4fff2",
+    "url": "/Sihaclick/static/media/comment.0d937459.svg"
+  },
+  {
+    "revision": "7ad7f5f5518ec9445a71fc2bb33c86ba",
+    "url": "/Sihaclick/static/media/date.7ad7f5f5.svg"
+  },
+  {
+    "revision": "fc14c97e9950a0ed4bdcfccd7e6c6bd1",
+    "url": "/Sihaclick/static/media/doctor.fc14c97e.jpg"
+  },
+  {
+    "revision": "54b4328040f8301ab34b351121538795",
+    "url": "/Sihaclick/static/media/event.54b43280.jpg"
+  },
+  {
+    "revision": "11a8f6f98ff6cbe0335eb61427e19c81",
+    "url": "/Sihaclick/static/media/facebook.11a8f6f9.svg"
+  },
+  {
+    "revision": "f3fce399b15689122d3a4e187f69770d",
+    "url": "/Sihaclick/static/media/facebook_login.f3fce399.svg"
+  },
+  {
+    "revision": "40eab08f535ea6715e320406be544cc6",
+    "url": "/Sihaclick/static/media/filter.40eab08f.svg"
+  },
+  {
+    "revision": "b93a9936dd47295fc3e0fca66f9f1bdb",
+    "url": "/Sihaclick/static/media/google_icon.b93a9936.svg"
+  },
+  {
+    "revision": "bfcef8f7b48211aa176d183ba5697fc5",
+    "url": "/Sihaclick/static/media/heart.bfcef8f7.svg"
+  },
+  {
+    "revision": "ace1ffdd5579167d5d9f17c94242ab9d",
+    "url": "/Sihaclick/static/media/hero_annuaire.ace1ffdd.svg"
+  },
+  {
+    "revision": "de0edfd3ce7268ea582c094690cce394",
+    "url": "/Sihaclick/static/media/hero_section.de0edfd3.png"
+  },
+  {
+    "revision": "2dbae96c654bb6cca9dc33b3cd493beb",
+    "url": "/Sihaclick/static/media/home.2dbae96c.png"
+  },
+  {
+    "revision": "60be8cca7531c0b4a6c9c1b7b8fe2102",
+    "url": "/Sihaclick/static/media/instagram.60be8cca.svg"
+  },
+  {
+    "revision": "e3b6ebb33759c0e6d12dd0bc4f92ee01",
+    "url": "/Sihaclick/static/media/logo.e3b6ebb3.svg"
+  },
+  {
+    "revision": "fdd7182f54dab559c6821c09c6e87dae",
+    "url": "/Sihaclick/static/media/new.fdd7182f.jpg"
+  },
+  {
+    "revision": "2d630f768bdc518b329c7570e8cee1be",
+    "url": "/Sihaclick/static/media/notification.2d630f76.svg"
+  },
+  {
+    "revision": "375fad9a624c4e81685e911bdef05054",
+    "url": "/Sihaclick/static/media/o.375fad9a.svg"
+  },
+  {
+    "revision": "fe3cf7e23ff785ef8966f1fe9103e3d9",
+    "url": "/Sihaclick/static/media/plaquette.fe3cf7e2.svg"
+  },
+  {
+    "revision": "1fd2cc602153bd75605d126de57394fb",
+    "url": "/Sihaclick/static/media/pub.1fd2cc60.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Sihaclick/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Sihaclick/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Sihaclick/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Sihaclick/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d9217006bfb3bdfbfffc418f6c64eacd",
+    "url": "/Sihaclick/static/media/stage_hero.d9217006.png"
+  },
+  {
+    "revision": "3a4a0b05ec955e6b14f5227e765ba271",
+    "url": "/Sihaclick/static/media/stage_user.3a4a0b05.png"
+  },
+  {
+    "revision": "cc11305d1c85762b056012e1dac295be",
+    "url": "/Sihaclick/static/media/teva.cc11305d.png"
+  },
+  {
+    "revision": "d1cb0018ca42fdbcb227f32e26f41655",
+    "url": "/Sihaclick/static/media/toggle.d1cb0018.svg"
+  },
+  {
+    "revision": "ff0c36027f74d038ffcc96f9740458f8",
+    "url": "/Sihaclick/static/media/twitter.ff0c3602.svg"
+  },
+  {
+    "revision": "49f4c866573425c54b4cbc071de0a4b6",
+    "url": "/Sihaclick/static/media/upload.49f4c866.svg"
+  }
+]);
